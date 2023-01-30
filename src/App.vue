45efa78dd8410cd564f5f99ed4ed8f03670f8bb6
kimsujin <sooojiny@gmail.com>
2023-01-30 21:56:09 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="text-lg bg-gray-900 text-white ml-10 mt-4 pl-10 pt-3 hover:bg-gray-600">hello world</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
