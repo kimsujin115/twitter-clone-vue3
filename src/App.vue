0eb2cff53ca57ee4852b32b979c2342097816090
kimsujin <sooojiny@gmail.com>
2023-01-31 21:49:06 +0900

    트위터 사이드메뉴, 프로필 버튼

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,88 @@
 <template>
-    <h1 class="text-lg bg-gray-900 text-white ml-10 mt-4 pl-10 pt-3 hover:bg-gray-600">hello world</h1>
+  <div class="flex h-screen">
+    <div class="w-1/5 my-5 px-5 min-w-300 flex flex-col justify-between">
+      <div>
+        <!-- twitter logo -->
+        <i class="fa-brands fa-twitter text-3xl text-primary ml-4"></i>
+        <!-- sidemenu icons -->
+        <nav class="mt-3">
+          <ul class="flex flex-col items-start">
+            <li>
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                <i class="fas fa-house fa-fw text-2xl"></i>
+                <span class="ml-5 text-lg">홈</span>
+              </div>
+            </li>
+            <li>
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                <i class="far fa-hashtag fa-fw text-2xl"></i>
+                <span class="ml-5 text-lg">탐색하기</span>
+              </div>
+            </li>
+            <li>
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                <i class="far fa-bell fa-fw text-2xl"></i>
+                <span class="ml-5 text-lg">알림</span>
+              </div>
+            </li>
+            <li>
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                <i class="far fa-envelope fa-fw text-2xl"></i>
+                <span class="ml-5 text-lg">쪽지</span>
+              </div>
+            </li>
+            <li>
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                <i class="fa-regular fa-fw fa-bookmark text-2xl"></i>
+                <span class="ml-5 text-lg">북마크</span>
+              </div>
+            </li>
+            <li>
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                <i class="far fa-list-alt fa-fw text-2xl"></i>
+                <span class="ml-5 text-lg">리스트</span>
+              </div>
+            </li>
+            <li>
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                <i class="far fa-user fa-fw text-2xl"></i>
+                <span class="ml-5 text-lg">프로필</span>
+              </div>
+            </li>
+            <li>
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+                <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
+                <span class="ml-5 text-lg">더보기</span>
+              </div>
+            </li>
+          </ul>
+        </nav>
+        <!--// sidemenu icons -->
+
+        <!-- tweet button -->
+        <div>
+          <button class="mt-3 bg-primary text-white w-full h-12 rounded-full hover:bg-dark">트윗</button>
+        </div>
+        <!--// tweet button -->
+      </div>
+
+      <!-- profile button -->
+      <div class="mb-3">
+        <button class="mt-3 px-2 py-1 w-full h-12 rounded-full hover:bg-blue-50 flex items-center">
+          <img src="http://picsum.photos/100" alt="" class="w-10 h-10 rounded-full">
+          <div class="ml-2">
+            <div clas="text-sm font-bold">sujin.com</div>
+            <div class="text-xs text-grey-500 text-left" >@sujin</div>
+          </div>
+          <i class="ml-auto fas fa-ellipsis-h fa-fw text-2xl"></i>
+        </button>
+      </div> 
+      <!--// profile button -->
+    </div>
+
+    <!-- right -->
+    <div class="flex-1 bg-red-200"></div>
+  </div>
 </template>
 
 <script>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,20 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      /* tailwind 에다가 내가 원하는 스타일 지정 할 수 있음 */
+      minWidth : {
+        '100' : '100px',
+        '200' : '200px',
+        '300' : '300px'
+      },
+      colors : {
+        primary: '#1DA1F2',
+        dark: '#1a91da',
+        light: '#a5d0f5',
+        lighter : '#d4e5f4'
+      }
+    },
   },
   plugins: [],
 }
