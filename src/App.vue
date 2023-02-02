c9967c07e48e706004960d03ed75ed85cda67161
kimsujin <sooojiny@gmail.com>
2023-02-02 21:16:34 +0900

    트위팅 섹션 작업

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex h-screen">
     <!-- side section -->
-    <div class="w-1/4 py-5 px-5 flex flex-col justify-between border-r border-gray-200">
+    <div class="w-1/4 py-5 px-5 flex flex-col justify-between border-r border-gray-100">
       <div class="flex flex-col items-center lg:items-start">
         <!-- twitter logo -->
         <i class="fa-brands fa-twitter text-3xl text-primary lg:ml-4"></i>
@@ -87,8 +87,38 @@
       <!--// profile button -->
     </div>
 
-    <!-- right -->
-    <div class="flex-1 bg-red-200"></div>
+    <!-- right home page-->
+    <div class="flex-1">
+        <div class="flex h-screen">
+            <!-- main section -->
+            <div class="flex-1 border-r border-gray-100">
+              <div class="flex flex-col">
+                <!-- page title -->
+                <div class="border-b border-gray-100 px-3 py-2 font-bold text-lg">
+                  홈
+                </div>
+                <!-- trend section -->
+                <div class="flex px-3 py-3 border-b-8 border-grey-100">
+                  <img src="http://picsum.photos/200" alt="" class="w-10 h-10 rounded-full hover:opacity-80 cursor-pointer">
+                  <div class="ml-2 flex-1 flex flex-col">
+                    <textarea placeholder="무슨 일이 일어나고 있나요?" class="w-full text-lg font-bold foucs:outline-none mb-3 resize-none"></textarea>
+                    <div class="text-right">
+                      <button class="bg-primary text-sm font-bold text-white px-4 py-2 rounded-full hover:bg-dark">
+                        트윗
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <!-- tweeting section -->
+                 
+              </div>
+            </div>
+            
+            <div class="w-2/5">
+              
+            </div> 
+        </div>
+    </div>
   </div>
 </template>
 
