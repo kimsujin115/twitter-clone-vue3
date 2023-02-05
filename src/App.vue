b8752abc8871f4a96e3c7ed23408fe0b99a12d07
kimsujin <sooojiny@gmail.com>
2023-02-05 23:17:46 +0900

    우측 트렌드 영역 작업

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex h-screen">
+  <div class="flex h-screen container mx-auto">
     <!-- side section -->
-    <div class="w-1/4 py-5 px-5 flex flex-col justify-between border-r border-gray-100">
+    <div class="w-20 lg:w-1/4 py-5 px-5 flex flex-col justify-between border-r border-gray-100">
       <div class="flex flex-col items-center lg:items-start">
         <!-- twitter logo -->
         <i class="fa-brands fa-twitter text-3xl text-primary lg:ml-4"></i>
@@ -9,49 +9,49 @@
         <nav class="mt-3">
           <ul class="flex flex-col items-start">
             <li>
-              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
                 <i class="fas fa-house fa-fw text-2xl"></i>
                 <span class="ml-5 text-lg hidden lg:inline-block">홈</span>
               </div>
             </li>
             <li>
-              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
                 <i class="far fa-hashtag fa-fw text-2xl"></i>
                 <span class="ml-5 text-lg hidden lg:inline-block">탐색하기</span>
               </div>
             </li>
             <li>
-              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
                 <i class="far fa-bell fa-fw text-2xl"></i>
                 <span class="ml-5 text-lg hidden lg:inline-block">알림</span>
               </div>
             </li>
             <li>
-              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
                 <i class="far fa-envelope fa-fw text-2xl"></i>
                 <span class="ml-5 text-lg hidden lg:inline-block">쪽지</span>
               </div>
             </li>
             <li>
-              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
                 <i class="fa-regular fa-fw fa-bookmark text-2xl"></i>
                 <span class="ml-5 text-lg hidden lg:inline-block">북마크</span>
               </div>
             </li>
             <li>
-              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
                 <i class="far fa-list-alt fa-fw text-2xl"></i>
                 <span class="ml-5 text-lg hidden lg:inline-block">리스트</span>
               </div>
             </li>
             <li>
-              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
                 <i class="far fa-user fa-fw text-2xl"></i>
                 <span class="ml-5 text-lg hidden lg:inline-block">프로필</span>
               </div>
             </li>
             <li>
-              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full">
+              <div class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
                 <i class="fas fa-ellipsis-h fa-fw text-2xl"></i>
                 <span class="ml-5 text-lg hidden lg:inline-block">더보기</span>
               </div>
@@ -147,8 +147,59 @@
               </div>
             </div>
             <!-- trend section -->
-            <div class="w-2/5">
-              
+            <div class="hidden lg:block w-2/5 px-4 py-2">
+              <!-- tweet search input -->
+              <div class="relative mb-5">
+                <input type="input" placeholder="트위터 검색" class="pl-10 rounded-full w-full p-2 bg-gray-100 text-sm focus:ring-primary focus:ring-1 focus:bg-white focus:outline-none" />
+                <i class="fas fa-search absolute left-0 mt-2 ml-3 text-sm text-light"></i>
+              </div>
+              <!-- trend for me -->
+              <div class="w-full rounded-xl bg-gray-50">
+                <div class="flex items-center justify-between px-3 py-1 border-b border-gray-100">
+                  <strong class="text-lg">나를 위한 트렌드</strong>
+                  <i class="fas fa-cog text-lg text-primary hover:bg-blue-50 p-2 rounded-full text-center cursor-pointer"></i>
+                </div>
+                <ul>
+                  <li class="px-4 py-2 border-b border-gray-100 hover:bg-gray-200 cursor-pointer" v-for="trand in 3" :key="trand">
+                    <div class="flex">
+                      <div class="flex-1 flex flex-col">
+                        <span class="text-gray-500">대한민국에서 트렌드 중</span>
+                        <span class="font-bold text-lg">러블리즈</span>
+                        <span class="text-gray-500">3,564 트윗</span>
+                      </div>
+                      <i class="fas fa-ellipsis-h text-gray-500"></i>
+                    </div>
+                  </li>
+                </ul>
+                <div class="px-4 py-2 rounded-xl hover:bg-gray-100 cursor-pointer">
+                  <span class="text-primary text-lg rounded-xl">더보기</span>
+                </div>
+              </div>
+              <!-- follow recommends -->
+              <div class="mt-5 w-full rounded-xl bg-gray-50">
+                <div class="px-3 py-3 border-b border-gray-100">
+                  <strong class="text-lg">팔로우 추천</strong>
+                </div>
+                <ul>
+                  <li class="px-4 py-2 border-b border-gray-100 hover:bg-gray-200" v-for="trand in 3" :key="trand">
+                    <div class="flex items-center">
+                      <div class="mr-2">
+                        <img src="http://picsum.photos/200" class="w-12 h-12 rounded-full">
+                      </div>
+                      <div class="flex-1 flex flex-col">
+                        <span class="font-bold text-lg">트렌드</span>
+                        <span class="text-gray-500">@trand_korea</span>
+                      </div>
+                      <div>
+                        <button class="px-4 py-1 border-2 border-primary rounded-full text-primary hover:bg-blue-100">팔로우</button>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+                <div class="px-4 py-2 rounded-xl hover:bg-gray-100 cursor-pointer">
+                  <span class="text-primary text-lg rounded-xl">더보기</span>
+                </div>
+              </div>
             </div> 
         </div>
     </div>
