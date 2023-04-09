af6b947945574c4fe45740c3c1302396b9869485
kimsujin <sooojiny@gmail.com>
2023-04-09 21:03:46 +0900

    프로필 페이지 - 데이터 연결

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -8,15 +8,15 @@
                     <i class="fas fa-arrow-left text-primary p-3 rounded-full hover:bg-blue-50"></i>
                 </button>
                 <div>
-                    <div class="font-extrabold text-lg">sujin.com</div>
-                    <div class="text-xs text-gray-500">309 트윗</div>
+                    <div class="font-extrabold text-lg">{{ currentUser.email }}</div>
+                    <div class="text-xs text-gray-500">{{ currentUser.num_tweets }} 트윗</div>
                 </div>
             </div>
             <!-- profile area -->
             <div class="bg-gray-300 h-40 relative flex-none">
                 <!-- profile image -->
                 <div class="absolute -bottom-14 left-2 w-28 h-28 border-4 border-white bg-gray-100 rounded-full">
-                    <img src="http://picsum.photos/200" class="rounded-full opacity-90 hover:opacity-100 cursor-pointer" alt="">
+                    <img :src="currentUser.profile_img_url" class="rounded-full opacity-90 hover:opacity-100 cursor-pointer" alt="">
                 </div>
             </div>
             <!-- profile edit button -->
@@ -25,16 +25,16 @@
             </div>
             <!-- user info -->
             <div class="mx-3 mt-2">
-               <div class="font-extrabold text-lg">sujin.com</div>
-               <div class="text-gray">@sujin</div>
+               <div class="font-extrabold text-lg">{{ currentUser.emaild }}</div>
+               <div class="text-gray">@{{ currentUser.username }}</div>
                <div>
                 <span class="text-gray">가입일 : </span>
-                <span>2023년 2월</span>
+                <span>{{ moment(currentUser.created_at).format('YYYY년 MM월') }}</span>
                </div> 
                <div>
-                <span class="font-bold mr-1">28</span>
+                <span class="font-bold mr-1">{{ currentUser.followings.length }}</span>
                 <span class="text-gray">팔로우 중</span>
-                <span class="font-bold mr-1 ml-2">7</span>
+                <span class="font-bold mr-1 ml-2">{{ currentUser.follwers.length}}</span>
                 <span class="text-gray">팔로워</span>
                </div>
             </div>
@@ -55,7 +55,7 @@
             </ul>
             <!-- tweet -->
             <div class="overflow-y-auto">
-                <Tweet v-for="tweet in 10" :key="tweet" />
+                <Tweet v-for="tweet in 10" :key="tweet.id" :currentUser="currentUser" :tweet="tweet"  />
             </div>
         </div>
         <!-- trend section -->
@@ -66,8 +66,42 @@
 <script>
     import Trends from '../components/Trends.vue'
     import Tweet from '../components/Tweet.vue'
+    import store from '../store';
+    import { computed, ref, onBeforeMount } from 'vue';
+    import { TWEET_COLLECTION, USER_COLLECTION } from '../firebase';
+    import getTweetInfo from '../utils/getTweetInfo';
+    import moment from 'moment';
+
     export default {
         components : { Trends, Tweet },
+        setup() {
+            const currentUser = computed(() => store.state.user )
+            const tweets = ref([])
+
+            onBeforeMount(() => {
+
+                USER_COLLECTION.doc(currentUser.value.uid).onSnapshot(doc => {
+                    store.commit("SET_USER", doc.data())
+                })
+
+                TWEET_COLLECTION.where("uid", "==", currentUser.value.uid).orderBy("create_at", "desc").onSnapshot(snapshot => {
+                    snapshot.docChanges().forEach(async (change) => {
+                        let tweet = await getTweetInfo(change.doc.data(), currentUser.value); //getTweetInfo함수를 async로 가져왔으니까 해당 변수해서도 정보 가져올동안 기다려야함
+                        //console.log('tweet info : ', tweet)
+
+                        if (change.type === 'added') {
+                            tweets.value.splice(change.newIndex, 0, tweet)
+                        } else if(change.type === 'modified') {
+                            tweets.value.splice(change.oldIndex, 1, tweet)
+                        } else if(change.type == 'removed') {
+                            tweets.value.splice(change.oldIndex, 1)
+                        }
+                    })
+                })
+            })
+
+            return { currentUser, tweets, moment }
+        }
     }
 </script>
 
