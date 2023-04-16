daf1f078a8f90e93fa1b00b7aaf378bd08eee82f
kimsujin <sooojiny@gmail.com>
2023-04-16 14:35:30 +0900

    프로필 수정 모달 팝업 : 입력창 UI 생성

--- src/components/profileEditModal.vue
+++ src/components/profileEditModal.vue
@@ -18,7 +18,7 @@
                                 <button class="hover:bg-dark bg-primary text-white font-bold px-3 py-1 rounded-full">저장</button>
                             </div>
                         </div>
-                        <!-- tweeting section -->
+                        <!-- image section -->
                         <div class="h-60">
                             <!-- background image -->
                             <div class="bg-gray-300 h-40 relative flex-none flex items-center justify-center">
@@ -32,6 +32,23 @@
                                     <button @click="onChangeProfileImage" class="absolute bottom-9 left-9 h-10 w-10 hover:text-gray-200 rounded-full fas fa-camera text-white text-lg"></button>
                                     <input @change="previewProfileImage" type="file" accept="image/*" id="profileImageInput" class="hidden">
                                 </div>
+                            </div>
+                        </div>
+
+                        <!-- -->
+                        <div class="flex flex-col p-2">
+                            <div class="mx-2 my-2 px-2 py-1 border border-gray-200 text-gray rounded hover:border-primary hover:text-primary">
+                                <div class="text-sm">이름</div>
+                                <input type="text" :value="currentUser.email" class="text-black focus:outline-none">
+                            </div>
+                            <div class="mx-2 my-1 px-2 py-5 border border-gray-200 text-gray rounded hover:border-primary hover:text-primary">
+                                <input type="text" placeholder="자기소개" class="text-black focus:outline-none">
+                            </div>
+                            <div class="mx-2 my-1 px-2 py-5 border border-gray-200 text-gray rounded hover:border-primary hover:text-primary">
+                                <input type="text" placeholder="위치" class="text-black focus:outline-none">
+                            </div>
+                            <div class="mx-2 my-1 px-2 py-5 border border-gray-200 text-gray rounded hover:border-primary hover:text-primary">
+                                <input type="text" placeholder="웹사이트" class="text-black focus:outline-none">
                             </div>
                         </div>
                     </div>
